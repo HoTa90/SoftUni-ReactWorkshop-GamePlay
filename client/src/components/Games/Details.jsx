84d08f5ee6b59fc1c6d74ed1a2a51b06af7ac1f1
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-03-10 18:11:05 +0200

    Update Details.jsx

--- client/src/components/Games/Details.jsx
+++ client/src/components/Games/Details.jsx
@@ -7,23 +7,22 @@ export default function Details() {
   const [comments, setComments] = useState([])
   const { id } = useParams();
   const { data: game, isPending, error, del } = useFetch(`/data/games/${id}`);
-  const {post} = useFetch();
+  const { post } = useFetch();
   const { userData } = useAuth();
   const navigate = useNavigate()
-  
- 
+
+
   useEffect(() => {
     const controller = new AbortController()
     const fetchCommments = async () => {
       try {
         const response = await fetch(`http://localhost:3030/data/comments?where=gameId%3D%22${id}%22`);
-        
+
         if (!response.ok) {
           throw new Error(`HTTP Error ${response.status}: ${response.statusText}`);
         }
-    
+
         const commentsData = await response.json();
-        console.log(commentsData)
         setComments(commentsData);
       } catch (err) {
         console.log("Error fetching comments:", err.message);
@@ -36,7 +35,7 @@ export default function Details() {
       controller.abort()
     }
 
-  },[id])
+  }, [id])
 
 
   const deleteHandler = async (e) => {
@@ -53,17 +52,17 @@ export default function Details() {
 
 
   const formHandler = async (formData) => {
-    const {comment} = Object.fromEntries(formData)
+    const { comment } = Object.fromEntries(formData)
     const postData = {
       gameId: id,
       comment
     }
 
     try {
-     const newComment = await post('/data/comments', postData)
-      setComments(prevComments => [...prevComments, newComment] )
+      const newComment = await post('/data/comments', postData)
+      setComments(prevComments => [...prevComments, newComment])
 
-    } catch (err){
+    } catch (err) {
       console.log(err.message)
     }
   }
@@ -85,19 +84,19 @@ export default function Details() {
         </p>
         <div className="details-comments">
           <h2>Comments:</h2>
-        {comments.length > 0 ?
+          {comments.length > 0 ?
             (<ul>
               {comments.map((c) => (
                 <li className="comment" key={c._id}>
-                <p>{c.comment}</p>
-              </li>
+                  <p>{c.comment}</p>
+                </li>
               ))}
 
             </ul>)
             :
             <p className="no-comment">No comments.</p>
-       }
-       </div>
+          }
+        </div>
 
         {/* <!-- Edit/Delete buttons ( Only for creator of this game )  --> */}
         {userData?.id === game?._ownerId && (
@@ -111,15 +110,15 @@ export default function Details() {
 
       {/* <!-- Bonus --> */}
       {/* <!-- Add Comment ( Only for logged-in users, which is not creators of the current game ) --> */}
-     {userData && userData?.id !== game?._ownerId &&
-     <article className="create-comment">
-     <label>Add new comment:</label>
-     <form className="form" action={formHandler}>
-       <textarea name="comment" placeholder="Comment......"></textarea>
-       <input className="btn submit" type="submit" value="Add Comment" />
-     </form>
-   </article>}
-      
+      {userData && userData?.id !== game?._ownerId &&
+        <article className="create-comment">
+          <label>Add new comment:</label>
+          <form className="form" action={formHandler}>
+            <textarea name="comment" placeholder="Comment......"></textarea>
+            <input className="btn submit" type="submit" value="Add Comment" />
+          </form>
+        </article>}
+
 
     </section>
   );
